c674ee007f0947b1cafe80b76d6da940b364b181
samke24-meet <bokutosam0@gmail.com>
2023-07-17 16:42:12 +0300

    still working on the bonuses

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-function changeBackgroundColor(color) {
-document.body.style.changeBackgroundColor = 'color';
+function change(color) {
+document.body.style.backgroundColor = color;
+}
+
+// function hi(){
+// 	if (button.id == 
+// }
+
+var button = document.getElementById("green");
+
+// console.log(button);
+
+button.onclick = function() {myFunction()};
+
+function myFunction() {
+	change("red");
+  // button.innerHTML = "YOU CLICKED ME!";
 }
 
